d9084bfaf39ed1ec24faa80ab05d87812600091f
Luminiscental <luminiscental01@gmail.com>
2024-09-02 14:02:34 +0100

    Remove orphan files

--- 13-conky/system.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-echo '<span face="DejaVu Sans Mono"><span foreground="#d99f57" size="xx-large">top</span><span foreground="#78824b">'
-echo ''
-display-sensors 53
-echo ''
-LINES=16 top -b -n 1 -w
-echo '</span></span>'
--- 13-conky/todo.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-echo '<span face="DejaVu Sans Mono"><span foreground="#d99f57" size="xx-large">todo</span><span foreground="#78824b">'
-echo ''
-cat ~/Documents/personal/todo.txt
-echo '</span></span>'
